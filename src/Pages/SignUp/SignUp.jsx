cd6dbe092a335dc77ef17a32d18a90f6ac07e529
webashraf <aliashraftamim@gmail.com>
2023-04-19 17:25:29 +0600

    add password validation

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [err, setErr] = useState("");
+
     const handleRegisterForm = e =>{
         e.preventDefault();
         const form = e.target;
@@ -9,8 +11,20 @@ const SignUp = () => {
         const password = form.password.value;
         const confirm = form.confirm.value;
         console.log(email, password, confirm);
+        if (password !== confirm) {
+            setErr("Password did not mathced!!!")
+            return;
+        }
+        else if(password.length < 8 || confirm.length < 8){
+            setErr("Password must be minimum 8 carecter")
+            console.log(email, password, confirm);
+            return;
+        }
 
-        form.reset()
+
+
+
+        // form.reset()
     }
 
 
@@ -22,6 +36,7 @@ const SignUp = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Register now!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className='text-orange-300'>{err}</p>
                         <p >All ready have an account? <Link className="btn btn-link" to={"/login"}>Log In now</Link></p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-9">
